abaca02b8e8ed84ba3d8b44df5c9f55b619dd717
tferre25 <theo@openroam-prg-lm-1-144-17.net.univ-paris-diderot.fr>
2020-09-15 10:18:57 +0200

    pdb dope parser file clean

--- src/parser_dope.py
+++ src/parser_dope.py
@@ -2,12 +2,17 @@
 #!/usr/bin/env python3
 
 import argparse
-import numpy as np
 
 def dope_parser(dope_file) : 
 
     '''
-    This function manage dope file isolating alpha carbon - alpha carbon
+    This function manage dope file isolating alpha carbon - alpha carbon pair energies
+    
+    Statistical potential values : 
+    Contains energy value for a pair of specific atoms at a given bin distance of 0.5 Angstroms.
+    First value is for a pair of atoms at a distance between 0.25 and 0.75 A. 
+    Bin distance reaches 15.0 A.
+
     It retrieves : 
     1 - energy value for each residue - residue
 
@@ -20,62 +25,29 @@ def dope_parser(dope_file) :
     '''
 
     with open(dope_file, "r") as fillin:
-        lines = fillin.readlines()
-        energy_value = {}
-        for line in lines : 
-            word = line.split(" ")
-            #print(dope_list) # check = OK
-            res_first = word[0].strip()
-            atom_first = word[1].strip()
-            res_sec = word[2].strip()
-            atom_sec = word[3].strip()
-            #print(atom_first) # check = OK
+        energy_value_dict = {}
+        for line in fillin : 
+            words_list = line.strip().split(" ")
+            res_first = words_list[0].strip()
+            atom_first = words_list[1].strip()
+            res_sec = words_list[2].strip()
+            atom_sec = words_list[3].strip()
             if atom_first == "CA" and atom_sec == "CA":
-                #print("we are good, condition OK") # check = OK
-                res_res = str(res_first + "-" + res_sec)
-                #print(res_res) #check = OK
-                energy_value[res_res] =\
-                np.array([float(word[4].strip()),
-                        float(word[5].strip()),
-                        float(word[6].strip()),
-                        float(word[7].strip()),
-                        float(word[8].strip()),
-                        float(word[9].strip()),
-                        float(word[10].strip()), 
-                        float(word[11].strip()),
-                        float(word[12].strip()), 
-                        float(word[13].strip()),
-                        float(word[14].strip()),
-                        float(word[15].strip()),
-                        float(word[16].strip()),
-                        float(word[17].strip()),
-                        float(word[18].strip()),
-                        float(word[19].strip()),
-                        float(word[20].strip()), 
-                        float(word[21].strip()),
-                        float(word[22].strip()), 
-                        float(word[23].strip()),
-                        float(word[24].strip()),
-                        float(word[25].strip()),
-                        float(word[26].strip()),
-                        float(word[27].strip()),
-                        float(word[28].strip()),
-                        float(word[29].strip()),
-                        float(word[30].strip()), 
-                        float(word[31].strip()),
-                        float(word[32].strip()), 
-                        float(word[33].strip())])
-        print(energy_value)
-        return(energy_value)
+                res_res = (res_first, res_sec)
+                bin_list = [float(i) for i in words_list[4:]]
+                energy_value_dict[res_res] = bin_list
+        #print(energy_value_dict[("ALA","ALA")])
+        return(energy_value_dict)
 
 
 if __name__ == "__main__":                  
-    parser = argparse.ArgumentParser("Manage the dope.par.txt file : isolate CA-CA and energy values")
-    parser.add_argument("dope_file", help = "dope.par.txt ", type=str)
+    parser = argparse.ArgumentParser("Manage the dope.par.txt file to isolate CA-CA pair energies")
+    parser.add_argument("-d", "--dope-file", help = "dope.par.txt ", type=str)
     args = parser.parse_args()
-    if args.dope_file[:] != 'dope.par.txt' :         
+    DOPE_FILE_NAME = args.pdb_file
+    if DOPE_FILE_NAME[:] != 'dope.par.txt' :         
         print("Please select only the file 'dope.par.txt' provided by Jean-Christophe Gelly")
     else: 
-        dope_file = dope_parser(args.dope_file)
+        dope_energy_value = dope_parser(DOPE_FILE_NAME)
 
 
--- src/pdb_parser.py
+++ src/pdb_parser.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python3
 
-'''
-Question pour lundi : 
-- les Class GitHub Adam
-- return
-- if __name__
-- def __init__
-- print() à chaque ligne sans exagération ++
-'''
-
-
-
 import argparse
 import numpy as np
-def pdb_parser(pdb_file) :
+
+
+def pdb_parser(pdb_file):
     """
-    This function manage pdb file isolating alpha carbons only.
+    This function manage pdb file isolating alpha carbons only (atome type).
+    
     It retrieves:
-    1. atom number
+    1. residu number
     2. x, y, z coordinates
 
     Args:
@@ -26,62 +18,29 @@ def pdb_parser(pdb_file) :
     Returns:
         Dictionnary: {atom_number : array[coord_x, coord_y, coord_z]}
     """
-    #print("je suis dans la fonction")
 
     with open(pdb_file, "r") as fillin:
-        atom_number = {}
-        coordinates = {}
-        #print(coordinates) #bug check = ok, empty dict
-
-        '''
-        #line_start = {}
-        #atom_type = {}
-        #res_type = {}
-        #chains = {}
-        #res_number = {}
-        #occupency = {}
-        #temperature_factors = {}
-        #element_symbol = {}
-        #atom_charge = {}
-        '''
-        
-        lines = fillin.readlines()
-        for line in lines:
-            #print(line[0:6].strip()) #bug check = ok
+        coordinates_dict = {}
+        for line in fillin :
             atom_type = line[12:16].strip()
-            #print(atom_type) #bug check = ok
-            if line[0:6].strip() == "ATOM" and (atom_type == "CA" or atom_type == "C1*"):
-                #print("on est dans la condition") #bug check = ok
-                atom_number = int(line[6:11].strip())
-                #print(atom_number) #bug check = ok
-                #line_start[atom_number] = line[0:6].strip()
-                coordinates[atom_number] =\
-                np.array([float(line[30:38].strip()),
-                    float(line[38:46].strip()),
-                    float(line[46:54].strip())])
-                #print(coordinates) #bug check = ok
-                #print(coordinates[atom_number])#bug check = ok
-               
-                '''
-                res_type[atom_number] = line[17:20].strip()
-                atom_type[atom_number] = atom_type
-                chains[atom_number] = line[21:22].strip()
-                res_number[atom_number] = int(line[22:26].strip())
-                occupency[atom_number] = float(line[54:60].strip())
-                temperature_factors[atom_number] = float(line[60:66].strip())
-                element_symbol[atom_number] = line[76:78].strip()
-                atom_charge[atom_number] = line[78:80].strip()
-                '''
-    print(coordinates) # bug check = youpi !    
-    return(coordinates)
-    
+            if line[0:6].strip() == "ATOM" and (
+                atom_type == "CA" or atom_type == "C1*"):
+                res_number = int(line[22:26].strip())
+                coordinates[res_number] =\
+                    np.array([float(line[30:38].strip()),
+                              float(line[38:46].strip()),
+                              float(line[46:54].strip())])
+    return(coordinates_dict)
 
-if __name__ == "__main__":                  
-    parser = argparse.ArgumentParser("Manage pdp files and isolate alpha carbons")
-    parser.add_argument("pdb_file", help = "file .pdb ", type=str)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "Manage pdb files to isolate alpha carbons")
+    parser.add_argument("-p", "--pdb-file", help="file .pdb is required ", type=str)
     args = parser.parse_args()
-    if args.pdb_file[-4:] != '.pdb' :         
+    PDB_FILE_NAME = args.pdb_file
+    if PDB_FILE_NAME[-4:] != '.pdb':
         print("Please select file with pdb extension")
-    else: 
-        pdb_file = pdb_parser(args.pdb_file)
-        #print(pdb_file) bug check
+    else:
+        pdb_coordinates = pdb_parser(PDB_FILE_NAME)
+        
