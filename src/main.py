d5640bc7bc3a22c3184cd36d7b6f5943ba106c91
tferre25 <theo.ferreira.med@gmail.com>
2020-09-16 21:16:50 +0200

    error

--- src/main.py
+++ src/main.py
@@ -10,94 +10,48 @@ USAGE
 __authors__ = ("Theo Ferreira", "Estelle Mariaux")
 __date__ = "2020/09"
 
-import alignment
-import distance
-import parsing
-
-import sys
-
-if __name__ == "__main__":
-    pdb_coordinates = parsing.pdb_parser('1n0a.pdb')
-    print(pdb_coordinates)
-    #dope_energy_value = parsing.dope_parser()
-    #print(dope_energy_value)
-    mat_dist = distance.distance_matrix(pdb_coordinates)
-    print(mat_dist)
-    list_AA = parsing.fasta_code('1n09.fasta')
-     
-    
-    matrice_dope_score = alignment.matrix_low_level(mat_dist,list_AA,"GLU", 5)
-    print(matrice_dope_score)
+from parsing import *
+from distance import *
+from alignment import *
 
 
-
-"""
-    parser = argparse.ArgumentParser(
-        "Manage pdb files to isolate alpha carbons")
+if __name__ == "__main__":                  
+    parser = argparse.ArgumentParser("This program aligns a sequence to a model \
+                                structure using threading using double dynamic program. \
+                                It is based on THREADER by Jones DT 1998")
     parser.add_argument("-p", "--pdb-file",
                         help="file .pdb is required ", type=str)
+    parser.add_argument("-d", "--dope-file", 
+                        help="dope.par.txt ", type=str)
+    parser.add_argument("-f", "--fasta-file",
+                        help="file .fasta is required ", type=str)
     args = parser.parse_args()
+
     PDB_FILE_NAME = args.pdb_file
-    if PDB_FILE_NAME[-4:] != '.pdb':
-        print("Please select file with pdb extension")
-    else:
-        pdb_coordinates = parsing.pdb_parser(PDB_FILE_NAME)
-    
-    parser = argparse.ArgumentParser(
-        "Manage the dope.par.txt file to isolate CA-CA pair energies")
-    parser.add_argument("-d", "--dope-file", help="dope.par.txt ", type=str)
-    args = parser.parse_args()
     DOPE_FILE_NAME = args.dope_file
-    if DOPE_FILE_NAME[:] != 'dope.par.txt':
-        print(
-            + "Please select only the file 'dope.par.txt' provided by JC Gelly")
-    else:
-        dope_energy_value = parsing.dope_parser(DOPE_FILE_NAME)
+    FASTA_FILE_NAME = args.fasta_file
 
-if __name__ == "__main__":      
-            
-    parser = argparse.ArgumentParser("Manage pdp files and isolate alpha carbons")
-    parser.add_argument("pdb_file", help = "file .pdb ", type=str)
-    args = parser.parse_args()
-    if args.pdb_file[-4:] != '.pdb' :         
-        print("Please select file with pdb extension")
-    else: 
-        pdb_file = traitement_sequences.pdb_parser(args.pdb_file)
-        #print(pdb_file) #bug check
-        mat = matrice_threading.matrice_distance(pdb_file)
-        print(mat)
-
-#PDB
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        "Manage pdb files to isolate alpha carbons")
-    parser.add_argument("-p", "--pdb-file",
-                        help="file .pdb is required ", type=str)
-    args = parser.parse_args()
-    PDB_FILE_NAME = args.pdb_file
     if PDB_FILE_NAME[-4:] != '.pdb':
+        print("Please select file with pdb extension")
+    if DOPE_FILE_NAME[:] != 'dope.par.txt':
+        print("Please select the file 'dope.par.txt' provided by JC Gelly")
+    if FASTA_FILE_NAME[-6] != '.fasta' :
         print("Please select file with pdb extension")
     else:
         pdb_coordinates = pdb_parser(PDB_FILE_NAME)
+        dope_energy_value = dope_parser(DOPE_FILE_NAME)
+        seq_3_list = fasta_code(FASTA_FILE_NAME)
+        mat_dist = distance_matrix(pdb_coordinates)
+
+        print(matrix_low_level(distance_matrix, seq_3_list, seq_3_list[0], 1))
+        # dope_mat_matrix = []
 
 
-liste_AA = code_1_lettre_3_lettres('seq_cible.fasta')
-#print(liste_AA)
+        # for res in pdb_coordinates.keys():
+        #     for j in enumerate(seq_3_list):
+        #         dope_mat = matrix_low_level(distance_matrix, seq_3_list, j, j+1)
 
-dope_energy_value = dope_parser('dope.par.txt')
-pdb_file = pdb_parser('1n0a.pdb')
-#print(pdb_file) #bug check
-mat = matrice_distance(pdb_file)
-#print(mat)
-#print(len(mat[0]))
 
-for num_AA in range(len(liste_AA)):
-    matrice_dope_score = mat_dope_res_pos(mat,liste_AA,liste_AA[num_AA], 1)
 
-matrice_dope_score = mat_dope_res_pos(mat,liste_AA,"GLU", 9)
-#print(matrice_dope_score)
 
-for num_AA in range(len(liste_AA)):
-    for num_pos in range(1, 11):
-        matrice_dope_score = mat_dope_res_pos(mat, liste_AA, liste_AA[num_AA], num_pos)
-"""
+
